ead9e34a668f94996ddbc72cd4568ca1406b072e
Tutu-s <wjddns8387@gmail.com>
2025-04-24 00:29:02 +0900

    Update telegram_welcome.py

--- telegram_welcome.py
+++ telegram_welcome.py
@@ -1,17 +1,18 @@
 import telebot
+import os
+from dotenv import load_dotenv
 
-# 봇 토큰
-TOKEN = '7906505449:AAGQ2TRjt-Of1Ei7LWXbN-GKqXSlSrLvoYE'
+# .env 파일 불러오기
+load_dotenv()
 
-# Bot 객체 생성
+# 환경 변수에서 토큰 읽기
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-# 새 멤버 입장 이벤트 감지
 @bot.message_handler(content_types=['new_chat_members'])
 def greet_new_member(message):
     for new_user in message.new_chat_members:
-        welcome_message = f"👋 {new_user.first_name}님, 반갑습니다❣️\n비즈LIKE 동아리에 오신걸 환영합니다🎉\n모임 참석 전 상단에 고정돼있는 동아리 소개글을 필독해주세요🙏🙏"
+        welcome_message = f"👋 {new_user.first_name}님, 환영합니다! 🎉"
         bot.send_message(message.chat.id, welcome_message)
 
-# 봇 실행
 bot.infinity_polling()
