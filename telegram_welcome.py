0233b306595cd4e986a0cf650a1924f1093ae2ed
Tutu-s <wjddns8387@gmail.com>
2025-04-24 10:38:04 +0900

    Update telegram_welcome.py

--- telegram_welcome.py
+++ telegram_welcome.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=['new_chat_members'])
 def greet_new_member(message):
     for new_user in message.new_chat_members:
-        welcome_message = f"👋 {new_user.first_name}님, 환영합니다! 🎉"
+        welcome_message = f"👋 {new_user.first_name}님, 반갑습니다❣️\n비즈LIKE 동아리에 오신걸 환영합니다🎉\n모임 참석 전 상단에 고정돼있는 동아리 소개글을 필독해주세요🙏🙏"
         bot.send_message(message.chat.id, welcome_message)
 
 bot.infinity_polling()
